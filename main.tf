db3d4761dcd74c269977dbe6f951fe89669f92ad
PRATIKSHIRBHATE <pratikshirbhate@gmail.com>
2026-09-07 17:42:50 +0530

    Add S3 module and comment out EC2 resource for S3-only testing

--- main.tf
+++ main.tf
@@ -10,21 +10,54 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-01a00762f46d584a1"
-  instance_type = "t2.micro"
+# Temporarily commented out to test S3 module only
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-01a00762f46d584a1"
+#   instance_type = "t2.micro"
+#
+#   tags = {
+#     Name = "PSRemoteStateInstance"
+#   }
+# }
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 5.15"
+
+  bucket = "terraform-aws-pratik-module-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
 
   tags = {
-    Name = "PSRemoteStateInstance"
+    Name        = "PSModuleBucket"
+    ManagedBy   = "Terraform"
+    Environment = "dev"
   }
 }
 
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+# Temporarily commented out to test S3 module only
+# output "instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.app_server.id
+# }
+#
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.app_server.public_ip
+# }
+
+output "s3_bucket_id" {
+  description = "The name of the S3 bucket created by the module"
+  value       = module.s3_bucket.s3_bucket_id
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket created by the module"
+  value       = module.s3_bucket.s3_bucket_arn
 }
